43948ae1b05d2f19c9c1003ef2299791c2cd469c
Karla <karlitamile@hotmail.com>
2022-09-11 03:07:57 -0500

    Testing

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request){
         $imagen = $request->file('file');
-        return response()->json(['imagen'=> $imagen->extension()]);
+                //id unico para las imagenes
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+        
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000,1000);//fio tamaño
+
+        $imagenPath= public_path('uploads') . '/' . $nombreImagen; //creo ruta y nueva carpeta
+        $imagenServidor->save($imagenPath); //guardo
+
+        return response()->json(['imagen'=> $nombreImagen]);
     }
 }
